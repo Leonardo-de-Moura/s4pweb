68b8bd68cef69dc3e9bb013a599d5c82caee11b8
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-12-07 19:27:05 -0300

    up

--- scripts/page2.js
+++ scripts/page2.js
@@ -1,16 +1,26 @@
-
 const menuBtn = document.querySelector(".menu-btn");
 const nav = document.querySelector("nav");
 
+// As divs que precisam sumir quando o menu abrir
+const div1 = document.querySelector(".image-container");
+const div2 = document.querySelector(".title-container");
 
 menuBtn.addEventListener("click", (e) => {
   e.stopPropagation(); 
   menuBtn.classList.toggle("active");
   nav.classList.toggle("active");
-  
-  
-});
 
+  // Esconde/mostra as divs
+  const menuOpen = menuBtn.classList.contains("active");
+
+  if (menuOpen) {
+    div1.style.display = "none";
+    div2.style.display = "none";
+  } else {
+    div1.style.display = "";
+    div2.style.display = "";
+  }
+});
 
 document.addEventListener("click", (e) => {
   const clickedInsideMenu = nav.contains(e.target);
@@ -19,5 +29,9 @@ document.addEventListener("click", (e) => {
   if (!clickedInsideMenu && !clickedButton) {
     menuBtn.classList.remove("active");
     nav.classList.remove("active");
+
+    // Reexibe as divs quando o menu fechar
+    div1.style.display = "";
+    div2.style.display = "";
   }
 });
